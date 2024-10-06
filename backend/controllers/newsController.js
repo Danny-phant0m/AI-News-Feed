49b70a6b592afd54314a3cb3565c8ab948ddf726
unknown <dixondaniel434@gmail.com>
2024-10-06 18:57:04 +0200

    Filter news feed

--- backend/controllers/newsController.js
+++ backend/controllers/newsController.js
@@ -6,14 +6,19 @@ const hf = new HfInference(process.env.HUGGINGFACE_API_KEY);
 // Fetch and summarize news
 const fetchAndSummarizeNews = async (req, res) => {
     try {
-        const newsResponse = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}`);
+        const newsResponse = await axios.get(`https://newsapi.org/v2/top-headlines?language=en&apiKey=${process.env.NEWS_API_KEY}`);
         const articles = newsResponse.data.articles;
         const summaries = await Promise.all(articles.map(async (article) => {
             const summaryResponse = await hf.summarization({
                 model: 'facebook/bart-large-cnn',
                 inputs: `${article.title}. ${article.description}`,
             });
-            return { title: article.title, summary: summaryResponse.summary_text, url: article.url };
+            return { 
+                title: article.title, 
+                summary: summaryResponse.summary_text, 
+                url: article.url, 
+                category: article.category 
+            };
         }));
 
         res.json(summaries);
