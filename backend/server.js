b307f2d002ebbd21a1c0790afef378ca02e988ed
unknown <dixondaniel434@gmail.com>
2024-10-06 11:28:13 +0200

    Adding News route

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,10 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const newsRoutes = require('./routes/newsRoutes');
+
+app.use('/api', newsRoutes);
+
 
 // Start the server
 app.listen(PORT, () => {
