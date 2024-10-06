71f54a9b6f9f86d80f361a64f9de93092780ee68
unknown <dixondaniel434@gmail.com>
2024-10-06 11:44:53 +0200

    making app.js file

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NewsList from './components/NewsList';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>AI-Powered News Summary</h1>
+      <NewsList />
+    </div>
+  );
+}
+
+export default App;
